1254db3289ab0c7e4173e0e794cfb66fd6694a3d
Lee <plbrannan95@gmail.com>
2021-12-21 13:35:41 -0500

    Init and two-sum

---
+++ two-sum/main.ts
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/two-sum/
+// To execute TS directly in shell, use `ts-node main.ts`
+
+function twoSum(nums: number[], target: number): number[] {
+
+  for(let i: number = 0; i < nums.length; i++) {
+    for(let j: number = 0; j < nums.length; j++) {
+      if(nums[i] + nums[j] === target) return [i, j]
+    }
+  }
+
+  return null;
+}
+
+twoSum([2, 7, 11, 15], 9);
